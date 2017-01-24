e91b235f499668cc43a1f1ed99b4dba7c9ea9df2
BobbyPlitsch <robertpeitsch@gmx.net>
2017-01-24 18:35:36 +0100

    Full title-helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Returns the full title on a per-page baiss.
+
+  def full_title(page_title = '')
+    base_title = "43"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
