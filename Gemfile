7cf9f09c69fc4e969d9fc192d7ae4e79bc72d3f3
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-01 09:37:02 +0100

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 
+gem 'bcrypt'
+
 gem 'bootstrap-sass'
 
 gem 'rails-controller-testing'
--- example_user.rb
+++ example_user.rb
@@ -1,21 +1,13 @@
 class User
-  attr_accessor :first_name, :last_name, :email
+  attr_accessor :name, :email
 
   def initialize(attributes = {})
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
+    @name = attributes[:name]
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{full_name} <#{@email}"
+    "#{@name} <#{@email}>"
   end
 
-  def full_name
-    "#{@first_name} #{@last_name}"
-  end
-
-  def alphabetical_name
-    "#{@last_name}, #{@first_name}"
-  end
 end
