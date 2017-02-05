436769eaad1639d970bdd363e246efbeb5128f0c
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-05 17:03:43 +0100

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,16 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
       if @user.save
+        log_in @user
         flash[:success] = "Welcome Biatch"
         redirect_to @user
       else
         render "new"
       end
+  end
+
+  def destroy
+  
   end
 
 
